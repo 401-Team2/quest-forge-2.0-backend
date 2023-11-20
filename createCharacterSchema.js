a6ca4992bc1118f2b40bbf384411ba0caea2176d
joshuashea <joshshea44@gmail.com>
2023-11-20 11:49:27 -0800

    added to schema

--- createCharacterSchema.js
+++ createCharacterSchema.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 import mongoose from 'mongoose';
 const createcCharacterSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    race: String,
-    characterClass: String,
-
+  id: Number,
+  name: String,
+  age: Number,
+  race: String,
+  characterClass: String,
+  gender: String,
 });
-
